85ce569fadeaab101f476ccab3db7c9e28f039e3
mJekal <132838012+mJekal@users.noreply.github.com>
2023-09-29 23:40:03 +0900

    final exam

---
+++ 과제-복습/Palindrome.java
@@ -0,0 +1,74 @@
+package HW;
+
+// 기말 = 회문 검사 프로그램
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Palindrome extends JFrame {
+	JTextField text = new JTextField("");
+	JLabel label = new JLabel("Palindrome");
+	JButton bt = new JButton("회문검사");
+
+	public Palindrome() {
+		setTitle("회문검사 프로그램");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		text.setFocusable(true);
+		text.requestFocus();
+		text.addActionListener(new MyActionListener());
+
+		label.setFont(new Font("TimesRoman", Font.PLAIN, 50));
+		label.setForeground(Color.BLACK); // 글자색 변경
+		label.setHorizontalAlignment(JLabel.CENTER);
+
+		bt.addActionListener(new MyButtonListener());
+
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		c.add(text, BorderLayout.NORTH);
+		c.add(bt, BorderLayout.CENTER);
+		c.add(label, BorderLayout.SOUTH);
+
+		setSize(500, 300);
+		setVisible(true);
+	}
+
+	public void check() {
+
+		String input, reverse;
+		input = text.getText();
+		StringBuffer sb = new StringBuffer(input);
+		reverse = sb.reverse().toString();
+
+		if (input.equals(reverse)) {
+			label.setText("Palindrome");
+
+		} else
+			label.setText("No Palindrome");
+	}
+
+	private class MyActionListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			bt.setFocusable(true);
+			bt.requestFocus();
+
+		}
+
+	}
+
+	private class MyButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			check();
+
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Palindrome();
+	}
+
+}
